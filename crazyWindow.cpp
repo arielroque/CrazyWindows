fed5b28e8db6577b5726c5f7569d0d26f3d6418a
ariel <ariel.roque@hotmail.com>
2019-07-25 20:06:41 -0300

    Add code

---
+++ crazyWindow.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <windows.h>
+
+using namespace std;
+
+void crazyCursor(){
+	int x,y;
+	x = (rand()% 1000) +2;
+	y = (rand()% 1000) + 2;
+	SetCursorPos(x,y);
+}
+
+int main(){
+	int startTime,durationTime;
+	
+	cout << "Time to start in seconds: (Ex: 10)" << endl;
+	cin >> startTime;
+	cout << "Duration time in seconds: (Ex: 40)" << endl;
+	cin >> durationTime;
+	
+	auto startPreTime = chrono::steady_clock::now();
+	auto endPreTime = chrono::steady_clock::now();
+	int secondsPreTime = 0;
+	
+	while(secondsPreTime <= startTime){
+		endPreTime = chrono::steady_clock::now();
+		secondsPreTime = chrono::duration_cast<chrono::seconds>(endPreTime - startPreTime).count();
+	}
+	
+	auto start = chrono::steady_clock::now();
+	auto end = chrono::steady_clock::now();
+	int secondsElapseds = 0;
+	
+	while(secondsElapseds < durationTime){
+		end = chrono::steady_clock::now();
+		secondsElapseds = chrono::duration_cast<chrono::seconds>(end - start).count();
+		crazyCursor();
+		Beep(523,500); 
+	}
+	return 0;
+}
